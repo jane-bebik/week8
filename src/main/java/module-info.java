74d9a36ab6f51989b3bedbc75fe3f9d1e72029c8
Jane bebik <118847459+jane-bebik@users.noreply.github.com>
2024-11-07 18:45:38 -0500

    test

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.week8 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.week8 to javafx.fxml;
+    exports com.example.week8;
+}
